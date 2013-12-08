a9386d1f5c3dd193c679909fd58a6f4c25a52bd5
Aris FM <masoglek@gmail.com>
2013-12-08 22:10:19 +0700

    Demo pages github

---
+++ assets/js/webfont.js
@@ -0,0 +1,9 @@
+WebFont.load({
+	// Define your custom google font below.
+	google: { families: [ 
+		'Source+Sans+Pro:400,700,400italic,700italic:latin',
+		'PT+Serif:400,700,400italic,700italic:latin',
+		'Alegreya:400italic,700italic,400,700:latin',
+		'Montserrat:400,700:latin'
+	] }
+});
