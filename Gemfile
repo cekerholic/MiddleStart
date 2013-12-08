f85832a7432a20c0af747c48d739f0de94cfd81b
Aris FM <masoglek@gmail.com>
2013-12-08 17:43:35 +0700

    Init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+gem "middleman" , "~>3.2.0"
+gem "middleman-livereload",  "~>3.1.0"
+gem 'sprockets-media_query_combiner'
+
+# for windows only, comments gem below if not using windows and do "bundle update"
+gem "wdm"
---
+++ config.rb
@@ -0,0 +1,53 @@
+###
+# Page options, layouts, aliases and proxies
+
+# Alternative-layout
+# page "*-alt.html", :layout => :layout_alt
+
+# No-layout
+page "sitemap.html", :layout => false
+
+# Livereload Module
+# activate :livereload, :apply_js_live => false, :apply_css_live => false
+
+# Gather every @media-queries rule into one block
+require 'sass-media_query_combiner'
+
+# Compass config
+compass_config do |compass|
+  compass.line_comments = false
+end
+
+# Set Default Directory
+set :css_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/img'
+set :haml, { :attr_wrapper => "\"", :ugly => true }
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  compass_config do |compass|
+    compass.line_comments = false
+    # compass.enable_sourcemaps = false 
+    # compass.sass_options = {:quiet => true, :sourcemap => false}
+  end
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
