8fa837ee40125a56395d439d8514bc4e0a1ea498
Aris FM <masoglek@gmail.com>
2014-08-16 16:12:50 +0700

    Add HTML5 support for HAML & Bower configuration

--- config.rb
+++ config.rb
@@ -44,7 +44,18 @@ set :images_dir, 'assets/img'
 ###
 # Prevent HAML to indent or format the HTML output. This significantly improves rendering performance but makes viewing the source unpleasant.
 # If using Sublime Text, you can easily reindent the HTML output using this custom keybinding https://gist.github.com/cekerholic/8121537
-set :haml, { :attr_wrapper => "\"", :ugly => true }
+set :haml, {
+  :attr_wrapper => "\"",
+  :ugly => true,
+  :format => :html5
+}
+
+
+
+# Integrate bower
+after_configuration do
+  sprockets.append_path File.join "#{root}", "bower_components"
+end
 
 
 
